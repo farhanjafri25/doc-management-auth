401a84af49d31ca3fdf947facc6e6410660e2563
Farhan Jafri <farhanjafri@Farhans-MacBook-Air.local>
2025-03-17 00:58:02 +0530

    [Fixes] for ingestion queue

--- src/modules/ingestion-module/ingestion-module.ts
+++ src/modules/ingestion-module/ingestion-module.ts
@@ -5,11 +5,17 @@ import { IngestionService } from "./services/ingestion.service";
 import { IngestionRepository } from "./repositories/ingestion.repository";
 import { AccessTokenStrategy, RefreshTokenStrategy } from "src/strategies";
 import { BullModule } from "@nestjs/bull";
+import { IngestionProcessor } from "./processor/ingestion.processor";
 
 @Module({
-    imports: [JwtModule.register({secret: `${process.env.JWT_SECRET}`}), BullModule.registerQueue({name: 'ingestionQueue'})],
+    imports: [JwtModule.register({secret: `${process.env.JWT_SECRET}`}), BullModule.registerQueue({name: 'ingestionQueue',
+         redis: {
+            host: process.env.REDIS_HOST ||  'localhost',
+            port: Number(process.env.REDIS_PORT) || 6379
+         }
+    })],
     controllers: [IngestionController],
-    providers: [IngestionService, IngestionRepository, AccessTokenStrategy, RefreshTokenStrategy],
-    exports: [IngestionService, IngestionRepository, AccessTokenStrategy, RefreshTokenStrategy]
+    providers: [IngestionService, IngestionRepository, AccessTokenStrategy, RefreshTokenStrategy, IngestionProcessor],
+    exports: [IngestionService, IngestionRepository, AccessTokenStrategy, RefreshTokenStrategy, IngestionProcessor]
 })
 export class IngestionModule {}
--- src/modules/ingestion-module/processor/ingestion.processor.ts
+++ src/modules/ingestion-module/processor/ingestion.processor.ts
@@ -1,26 +1,28 @@
 import { OnQueueCompleted, OnQueueFailed, Process, Processor } from "@nestjs/bull";
 import { Job } from "bullmq";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import { IngestionEntity } from "../entities/ingestion.entity";
 import { IngestionStatus } from "../enums/ingestion-status.enum";
 
 @Processor('ingestionQueue')
 export class IngestionProcessor {
+    private inggestionRepo: Repository<IngestionEntity>
     constructor(
-        @InjectRepository(IngestionEntity)
-        private inggestionRepo: Repository<IngestionEntity>
-    ) {}
+        private db: DataSource
+    ) {
+        this.inggestionRepo = this.db.getRepository(IngestionEntity);
+    }
 
     @Process('triggerIngestion')
-    public async handleIngestion(processJob : Job<{documentId: string, filePath: string}>) {
+    public async handleIngestion(processJob: Job<{ documentId: string, filePath: string }>) {
         const { documentId, filePath } = processJob.data;
         //Example format for calling the python webhook
         try {
             const pythonUrl = ""
             // await axios.post(pythonUrl, {documentId, filePath});
             console.log(`documentId`, documentId, `filepath`, filePath);
-            
+
         } catch (error) {
             console.log(`error in ingestion api`);
         }
@@ -39,8 +41,8 @@ export class IngestionProcessor {
     async onFailed(job: Job, error: Error) {
         const { documentId } = job.data;
         await this.inggestionRepo.update(
-            {documentId: documentId},
-            {ingestionStatus: IngestionStatus.FAILED}
+            { documentId: documentId },
+            { ingestionStatus: IngestionStatus.FAILED }
         )
     }
 }
